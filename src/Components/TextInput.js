f6bd821f87da9791c3ee0afd6f9e72464ce8a231
Aada (Liu Ran) <aadaappelsiini@gmail.com>
2022-08-22 14:49:50 +0300

    added input state

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,7 +1,16 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+
+
 
 export default function TextInput() {
+  const [input, setInput] = useState('')
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(input)
+  }
   return (
     <Box
       component="form"
@@ -11,7 +20,14 @@ export default function TextInput() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="standard-basic" label="Standard" variant="standard" onChange={(e)=> {setInput(e.target.value)}}/>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      />
     </Box>
   );
 }
