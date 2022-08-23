74b12e1b18fb3f4b03ba6d0b3e60f64f77e9a75e
walumo <miikka.hakulinen@awacademy.training>
2022-08-23 10:50:49 +0300

    Azure AI cognitive services working

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import SearchPage from "./Components/SearchPage";
 import SpeechToText from "./Components/SpeechToText";
 import OutcomePage from './Components/OutcomePage';
 import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import SpeechInput from "./Components/SpeechInput";
 
 function App() {
   return (
     <div>
       <h1>Welcome to React Router!</h1>
-      <SearchPage />
-      <SpeechToText />
+      <SpeechInput/>
+
+      {/* <SearchPage />
+      <SpeechToText /> */}
     {/* <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
     </Routes> */}
-    <Routes>
+    {/* <Routes>
       <Route path="outcomepage" element={<OutcomePage />} />
-    </Routes>
+    </Routes> */}
   </div>
   );
 }
---
+++ src/Components/SpeechInput.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import createSpeechServicesPonyfill from 'web-speech-cognitive-services';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const SUBSCRIPTION_KEY = '53b4ab187c3d4fdc81515c0369724f3f';
+const REGION = 'northeurope';
+
+const { SpeechRecognition: AzureSpeechRecognition } = createSpeechServicesPonyfill({
+  credentials: {
+    region: REGION,
+    subscriptionKey: SUBSCRIPTION_KEY,
+  }
+});
+SpeechRecognition.applyPolyfill(AzureSpeechRecognition);
+
+const SpeechInput = () => {
+  const {
+    transcript,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  console.log(transcript)
+  const startListening = () => SpeechRecognition.startListening({
+    continuous: true,
+    language: 'en-US'
+  });
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <div>
+      <button onClick={startListening}>Start</button>
+      <button onClick={SpeechRecognition.abortListening}>Abort</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default SpeechInput;
