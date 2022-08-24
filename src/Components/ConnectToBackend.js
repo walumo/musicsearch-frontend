ed5a8a476a91a52dbba117ca8d8298a0a5b3c808
Erkki Rasi <erkkirasi@gmail.com>
2022-08-24 18:41:15 +0300

    HTTP Get request with Axios working

--- src/Components/ConnectToBackend.js
+++ src/Components/ConnectToBackend.js
@@ -1,27 +1,46 @@
-import {useState} from 'react';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 const ConnectToBackend = () => {
-    const [backendData, setbackendData] = useState();
-    
-    
-    fetch('https://api.npms.io/v2/search?q=react')
-        .then(response => response.json())
-        .then(data => setbackendData(data.total))       
-    
-  
-  
-    
+
+    const [backendData, setbackendData] = useState(0);
+
+    const axios = require('axios');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get(
+                'https://localhost:44326/Api/songs?q=thriller', 
+                );
+                setbackendData(result.data);
+
+            } catch (err) {
+                console.error(err);
+            }
+      
+          };
+          
+          fetchData();
+        }, []);
+
+    const printData = () => {
+        try {
+            return "Tulos backendistä: " + backendData.Result[0].Artist;
+
+        } catch {
+            return "Ei natsannut";
+        }
+        
+    }
+
     return (
         <div>
-        <h1>{backendData}</h1>
-        
-        </div>
+            <h2>{printData()}</h2>
 
-    )
-    // }
-  
-  }
-  
+        </div>
+    );
+    }
   
   export default ConnectToBackend;
 
--- src/Components/OutcomePage.js
+++ src/Components/OutcomePage.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import TextInput from './TextInput';
 import {useState} from 'react';
-import ConnectToBackend from './ConnectToBackend';
+
 
 
 
