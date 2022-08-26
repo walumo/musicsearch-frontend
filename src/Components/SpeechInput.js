1c55a1e48f176a1997faa7dfe349e26885a4974c
walumo <miikka.hakulinen@awacademy.training>
2022-08-26 13:27:01 +0300

    geoposition logging added

--- src/Components/SpeechInput.js
+++ src/Components/SpeechInput.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import createSpeechServicesPonyfill from 'web-speech-cognitive-services';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './components.css'
 import axios, { Axios } from 'axios';
 import OutcomePage from './OutcomePage';
+
+
 
 const SUBSCRIPTION_KEY = '53b4ab187c3d4fdc81515c0369724f3f';
 const REGION = 'northeurope';
@@ -17,6 +19,30 @@ const { SpeechRecognition: AzureSpeechRecognition } = createSpeechServicesPonyfi
 SpeechRecognition.applyPolyfill(AzureSpeechRecognition);
 
 const SpeechInput = () => {
+
+  let crd; //variable for coordinates
+
+  useEffect(()=> {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    
+    function success(pos) {
+      crd = pos.coords;
+      console.log(crd.latitude)
+      console.log(crd.longitude)
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  
+  },[])
+  
   const {
     transcript,
     resetTranscript,
