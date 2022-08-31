c240b45697f4d2142f7aef93d8af46e4db534d50
Erkki Rasi <erkkirasi@gmail.com>
2022-08-31 14:24:43 +0300

    added and refined info and about texts.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,14 +21,14 @@ const Footer = ({language}) => {
   let madeBy = 'Aada, Ilari, Jutta, Miikka H., Miikka S., Erkki';
 
   if (language === "en-US") {
-    howToTitle = 'How to';
-    howToDescription1 = 'Press the button to record your speech and do a search';
-    howToDescription2 = 'This is the second paragraph';
+    howToTitle = 'How to use Verse';
+    howToDescription1 = 'With Verse app you can search songs by song lyrics. You can use speech recognition or type the lyrics in the search field.';
+    howToDescription2 = 'Press the logo button to start and stop recording speech. You can change the app language which also changes speech recognition locale.';
     howToButtonText = 'How to'
 
     aboutTitle = 'About';
-    aboutDescription1 = 'Verse is made by:';
-    aboutDescription2 = madeBy;
+    aboutDescription1 = 'Verse is made by AW Academy alumnis ' + madeBy + '.';
+    aboutDescription2 = 'Verse is using Genius API and Spotify API';
     aboutButtonText = 'About';
 
   } else if (language === "fi-FI") {
@@ -38,8 +38,8 @@ const Footer = ({language}) => {
     howToButtonText = 'Ohjeet'
 
     aboutTitle = 'Tietoa';
-    aboutDescription1 = 'Versen ovat kehittäneet';
-    aboutDescription2 = madeBy;
+    aboutDescription1 = 'Versen ovat kehittäneet AW Academyn alumnit ' + madeBy + '.';
+    aboutDescription2 = 'Verse käyttää Genius API:a ja Spotify API:a.';
     aboutButtonText = 'Tietoa';
 
   } else if (language === "sv-SE") {
@@ -49,8 +49,8 @@ const Footer = ({language}) => {
     howToButtonText = 'Instruktioner'
 
     aboutTitle = 'Om Verse';
-    aboutDescription1 = 'Verse är tillverkad av ';
-    aboutDescription2 = madeBy;
+    aboutDescription1 = 'Verse är tillverkad av AW Academy alumner ' + madeBy + '.';
+    aboutDescription2 = 'Verse använder Genius API och Spotify API.';
     aboutButtonText = 'Om Verse';
   }
 
